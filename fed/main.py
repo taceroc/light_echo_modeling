6fb27427170e97eed39daedba63a0226bc2e2bc8
taceroc <51520204+taceroc@users.noreply.github.com>
2023-11-17 17:50:35 -0500

    Update main.py

--- fed/main.py
+++ fed/main.py
@@ -8,7 +8,7 @@ from astropy.cosmology import FlatLambdaCDM
 
 import sys
 # sys.path.append('/content/drive/MyDrive/LE2023/dust/code')
-sys.path.append(r"code/dust/code")
+sys.path.append(r"path_to_LE/dust/code")
 import var_constants as vc
 import dust_constants as dc
 import fix_constants as fc
@@ -16,7 +16,7 @@ import scattering_function as sf
 import size_dist as sd
 import calculate_scattering_function as csf
 
-sys.path.append(r"code")
+sys.path.append(r"path_to_LE")
 import surface_brightness as sb
 # import brightness as fb
 import geometry_nodelta as le_geo
@@ -38,7 +38,7 @@ for ct in [290, 320, 360, 440, 520]: #alpha = 0 done
     xmax = r_le-a*ctyi
     x = np.linspace(xmin,xmax,1000)
     new_xs_v838, new_ys_v838, surface_v838, act_v838, ange_v838, cossigma_v838 = le_geo.LE_xy_surface_concate_plane(alpha, vc.z0ly, ctyi, x)
-    path_ge = r"save_data/"
+    path_ge = r"path_to_LE/save_data/"
     np.save(path_ge+r"plane_nd_xs_v838_t%s.npy"%(ct), new_xs_v838)
     np.save(path_ge+r"plane_nd_ys_v838_t%s.npy"%(ct), new_ys_v838)
     np.save(path_ge+r"plane_nd_flux_v838_t%s.npy"%(ct),surface_v838)  
@@ -48,7 +48,7 @@ for ct in [290, 320, 360, 440, 520]: #alpha = 0 done
     le_pl.plot(new_xs_v838, new_ys_v838, surface_v838, alpha, act_v838, axes, fig, save = False, name = "name")
     axes.set_title(r"V838 Mon - Plane: time$_{obs}$ = %s days"%(ct))
     # axes.set_title(r"V838 Mon - Plane: $\alpha$ = %s deg "%(al))
-    name = r"figures/"+r"plane_nd_v838_t%s.pdf"%(ct)
+    name = r"path_to_LE/figures/"+r"plane_nd_v838_t%s.pdf"%(ct)
     plt.savefig(name, dpi = 700)
     # plt.show()
 print("end %s"%(ct))
@@ -63,7 +63,7 @@ for al in [0, 15, 45, 75, 120, 310]:
     xmax = r_le-a*vc.ct
     x = np.linspace(xmin,xmax,1000)
     new_xs_v838, new_ys_v838, surface_v838, act_v838, ange_v838, cossigma_v838 = le_geo.LE_xy_surface_concate_plane(al, vc.z0ly, vc.ct, x)
-    path_ge = r"save_data/"
+    path_ge = r"path_to_LE/save_data/"
     np.save(path_ge+r"plane_nd_xs_v838_a%s.npy"%(al), new_xs_v838)
     np.save(path_ge+r"plane_nd_ys_v838_a%s.npy"%(al), new_ys_v838)
     np.save(path_ge+r"plane_nd_flux_v838_a%s.npy"%(al), surface_v838)
@@ -73,7 +73,7 @@ for al in [0, 15, 45, 75, 120, 310]:
     le_pl.plot(new_xs_v838, new_ys_v838, surface_v838, al, act_v838, axes, fig, save = False, name = "name")
     # axes.set_title(r"V838 Mon - Plane: time$_{obs}$ = %s days, $\alpha$ = %s deg,  z0 = %s pc"%(vc.Deltat, al, vc.z0))
     axes.set_title(r"V838 Mon - Plane: $\alpha$ = %s deg "%(al))
-    name = r"figure/"+r"plane_nd_v838_a%s.pdf"%(al)
+    name = r"path_to_LE/figure/"+r"plane_nd_v838_a%s.pdf"%(al)
     plt.savefig(name, dpi = 700)
     # plt.show()
 print("end %s"%(al))
